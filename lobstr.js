ca5f84cea4b2e770f0c15c40c058e079b0a95db5
Lewis Powers <lpowers1985@gmail.com>
2022-08-22 11:47:46 -0700

    csv fn prints only what is displayed

--- lobstr.js
+++ lobstr.js
@@ -49,7 +49,9 @@ function csv() {
     let len = assetsArray.length;
     let array = [];
     array[0] = `name,code,asset_amount,currency_amount,domain,asset_issuer`;
-    for (let i = 0; i < len; i++) array.push(mergeInfoIntoCSVFormat(i));
+    for (let i = 0; i < len; i++) {
+      if (assetsArray[i].style.display !== 'none') array.push(mergeInfoIntoCSVFormat(i));
+    }
     let result = array.join(`\n`);
     console.log(result);
   }
@@ -67,86 +69,93 @@ function csv() {
 }
 
 function displayTotals() {
-  let filledAssetsCount = assetsArray.filter( asset => {
-    if (!asset.children[1].textContent.includes(' 0 ')) return asset;
-  }).length;
 
-  let unfilledAssetsCount = assetsArray.filter( asset => {
-    if (asset.children[1].textContent.includes(' 0 ')) return asset;
-  }).length;
+    displayTotals.hasRun = true;
+    let filledAssetsCount = assetsArray.filter( asset => {
+      if (!asset.children[1].textContent.includes(' 0 ')) return asset;
+    }).length;
 
-  let names = {
-    allAssetsCount: `Total Assets: ${assetsArray.length}`,
-    filledAssetsCount: `Filled Assets: ${filledAssetsCount}`,
-    unfilledAssetsCount: `Uncharted Assets: ${unfilledAssetsCount}`,
-    totalBalance: `Balance: ${symbol}${totalBalance}`
-  }
+    let unfilledAssetsCount = assetsArray.filter( asset => {
+      if (asset.children[1].textContent.includes(' 0 ')) return asset;
+    }).length;
 
-  let namesArray = Object.keys(names);
-  let titleEl = document.getElementsByClassName('title-extra')[1];
-  let assetInfoDiv = document.createElement('div');
-  assetInfoDiv.style.display = 'flex';
-  assetInfoDiv.style.justifyContent = 'space-around';
-  assetInfoDiv.style.marginTop = '8px';
-  titleEl.append(assetInfoDiv);
-  namesArray.forEach((fnName) => {
-    if (fnName === 'totalBalance') {
-      let el = createEl(fnName);
-      el.style.fontSize = '1.8em';
-      titleEl.append(el);
-    } else {
-      assetInfoDiv.append(createEl(fnName));
+    let names = {
+      allAssetsCount: `Total Assets: ${assetsArray.length}`,
+      filledAssetsCount: `Filled Assets: ${filledAssetsCount}`,
+      unfilledAssetsCount: `Uncharted Assets: ${unfilledAssetsCount}`,
+      totalBalance: `Balance: ${symbol}${totalBalance}`
     }
-  })
 
-  function createEl(content) {
-    let el = document.createElement('span');
-    el.style.display = 'block';
-    el.style.lineHeight = '2em';
-    el.style.fontSize = '1.5em';
-    el.textContent = names[content];
-    return el;
-  }
+    let namesArray = Object.keys(names);
+    let titleEl = document.getElementsByClassName('title-extra')[1];
+    let assetInfoDiv = document.createElement('div');
+    assetInfoDiv.style.display = 'flex';
+    assetInfoDiv.style.justifyContent = 'space-around';
+    assetInfoDiv.style.marginTop = '8px';
+    titleEl.append(assetInfoDiv);
+    namesArray.forEach((fnName) => {
+      if (fnName === 'totalBalance') {
+        let el = createEl(fnName);
+        el.style.fontSize = '1.8em';
+        titleEl.append(el);
+      } else {
+        assetInfoDiv.append(createEl(fnName));
+      }
+    })
+
+    function createEl(content) {
+      let el = document.createElement('span');
+      el.style.display = 'block';
+      el.style.lineHeight = '2em';
+      el.style.fontSize = '1.5em';
+      el.textContent = names[content];
+      return el;
+    }
+
 }
 
 function filterSystem() {
-  document.getElementsByClassName('title-extra')[1].style.marginBottom = '0';
-  let div = document.createElement('div');
-  div.classList.add('main-text');
-  div.style.marginTop = '20px';
-  div.innerText = 'Filter Your Assets List';
-  let filterInput = document.createElement('input');
-  filterInput.type = 'text';
-  filterInput.style.width = '100%';
-  filterInput.style.margin = '11px 0';
-  filterInput.setAttribute('placeholder', 'Filter by asset name, code, domain or issuer address');
-  let count = assetsArray.length;
-  let assetCountElement = document.createElement('span');
-  assetCountElement.innerText = `Listed Assets: ${count}`;
-  filterInput.addEventListener('input', event => {
-    count = 0;
-    let text = event.target.value.trim().toLowerCase();
-    event.preventDefault();
-    assetsArray.filter( (asset, i) => {
-      if (textIsIncluded(text, i)) {
-        asset.style.display = 'flex';
-        count++;
-      } else {
-        asset.style.display = 'none';
-      }
-    })
-    assetCountElement.innerText = `Filtered Assets: ${count}`;
-  });
-  document.querySelector('.form-group').style.display = 'none';
-  div.append(filterInput, assetCountElement)
-  document.getElementsByClassName('title-extra')[1].append(div);
-  function textIsIncluded(inputText, idx) {
-    return namesArray[idx].toLowerCase().includes(inputText) ||
-    codesArray[idx].toLowerCase().includes(inputText) ||
-    domainsArray[idx].toLowerCase().includes(inputText) ||
-    issuersArray[idx].toLowerCase().includes(inputText);
-  }
+
+    filterSystem.hasRun = true;
+    document.getElementsByClassName('title-extra')[1].style.marginBottom = '0';
+    let div = document.createElement('div');
+    div.classList.add('main-text');
+    div.style.marginTop = '20px';
+    div.innerText = 'Filter Your Assets List';
+    let filterInput = document.createElement('input');
+    filterInput.type = 'text';
+    filterInput.style.width = '100%';
+    filterInput.style.margin = '11px 0';
+    filterInput.setAttribute('placeholder', 'Filter by asset name, code, domain or issuer address');
+    let count = assetsArray.length;
+    let assetCountElement = document.createElement('span');
+    assetCountElement.innerText = `Listed Assets: ${count}`;
+    filterInput.addEventListener('input', event => {
+      count = 0;
+      let text = event.target.value.trim().toLowerCase();
+      event.preventDefault();
+      assetsArray.filter( (asset, i) => {
+        if (textIsIncluded(text, i)) {
+          asset.style.display = 'flex';
+          count++;
+        } else {
+          asset.style.display = 'none';
+        }
+      })
+      assetCountElement.innerText = `Filtered Assets: ${count}`;
+    });
+    document.querySelector('.form-group').style.display = 'none';
+    div.append(filterInput, assetCountElement)
+    document.getElementsByClassName('title-extra')[1].append(div);
+    function textIsIncluded(inputText, idx) {
+      return namesArray[idx].toLowerCase().includes(inputText) ||
+      codesArray[idx].toLowerCase().includes(inputText) ||
+      domainsArray[idx].toLowerCase().includes(inputText) ||
+      issuersArray[idx].toLowerCase().includes(inputText);
+    }
+
 }
+
 csv();
 displayTotals();
 filterSystem();
